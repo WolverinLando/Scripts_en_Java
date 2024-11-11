e1fa532d180c644689e86c5ff27098667f2e1d98
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-11 12:23:59 -0600

    Algoritmo que calcula la letra que te corresponde dependiendo de la calificacion

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -8,41 +8,27 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-    Scanner orlando = new Scanner(System.in);
-	// Datos de entrada
-    System.out.println("Ingresa la calificacion obtenida: (0-10)");
-    int calificacion = orlando.nextInt();
-	// Evaluamos el valor de entrada
-    switch (calificacion) {
-		case 0:
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-	     System.out.println("Tu calificacion es: F");
-        break;
-        case 6:
-        case 7:
-        System.out.println("Tu calificacion es: D");
-        break;
-        case 8:
-        System.out.println("Tu calificacion es: C");
-        break;
-        case 9:
-        System.out.println("Tu calificacion es: B");
-        break;
-        case 10:
-        System.out.println("Tu calificacion es: A");
-        break;
-        default:
-        System.out.println("Calificacion invalida");
-        break;
+        Scanner orlando = new Scanner(System.in);
+
+        // Datos de entrada
+        System.out.println("Ingresa la calificación obtenida: (0-10)");
+        int calificacion = orlando.nextInt();
+
+        // Evaluamos el valor de entrada
+        if (calificacion >= 0 && calificacion <= 5) {
+            System.out.println("Tu calificación es: F");
+        } else if (calificacion == 6 || calificacion == 7) {
+            System.out.println("Tu calificación es: D");
+        } else if (calificacion == 8) {
+            System.out.println("Tu calificación es: C");
+        } else if (calificacion == 9) {
+            System.out.println("Tu calificación es: B");
+        } else if (calificacion == 10) {
+            System.out.println("Tu calificación es: A");
+        } else {
+            System.out.println("Calificación inválida");
         }
     }
 }
-
-
-
 
 
