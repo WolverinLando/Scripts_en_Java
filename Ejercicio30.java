33f2d518c08b8efd98331437aec16b57b243823e
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-13 07:52:50 -0600

    Multipicacion de arreglos

--- Ejercicio30.java
+++ Ejercicio30.java
@@ -1,3 +1,4 @@
 /* Autor: Orlando Urbano Trejo (Lando)
 * Fecha: 18-07-2023
- * Correo: urbanoorlando79@gmail.com
+* Correo: urbanoorlando79@gmail.com
+* Ejercicio: Haciendo uso de 2 arreglos, almacena 5 números en cada arreglo,
