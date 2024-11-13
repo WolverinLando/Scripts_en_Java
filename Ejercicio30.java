27b81de7b3e03fc4747b1bdb876785f55097490e
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-12 20:18:14 -0600

    Multiplicacion de arreglos

--- Ejercicio30.java
+++ Ejercicio30.java
@@ -1,35 +1,44 @@
 /* Autor: Orlando Urbano Trejo (Lando)
  * Fecha: 18-07-2023
  * Correo: urbanoorlando79@gmail.com
- 
- * Ejercicio: Haciendo uso de 2 arreglos, almacena 5 números en cada arreglo, posteriormente vas a multiplicar los arreglos */
+ * Ejercicio: Haciendo uso de 2 arreglos, almacena 5 números en cada arreglo, 
+ * posteriormente vas a multiplicar los arreglos
+ */
 
 import java.util.Scanner;
 
-public class Ejercicio30{
-	public static void main(String[] args){
-		Scanner Orlando = new Scanner(System.in);
-		int[] Primero = new int[5];
-		int[] Segundo = new int[5];
-		int[] Resultado = new int[5];
-		int Suma;
-		
-		for(int i = 0; i < 5; i++){
-			 System.out.print(" Valor " + (i+1) + ": ");
-		    Primero[i] = Orlando.nextInt();
-      }
-		for(int i = 4; i >= 0; i-- ){
-			 Suma = i + 2;
-		    System.out.print(" Valor " + (Suma-1) + ": ");
-			 Segundo[i] = Orlando.nextInt();
+public class Ejercicio30 {
+    public static void main(String[] args) {
+        // Crear objeto Scanner para la entrada de datos
+        Scanner Orlando = new Scanner(System.in);
 
-		}
-		for(int i = 0; i < 5; i++){
-			 Resultado[i] = Primero[i] + Segundo[i];
-		}
-      for(int i = 0; i < 5; i++){
-		    System.out.print(" " + Resultado[i]);
-		}
+        // Crear 3 arreglos de tamaño 5
+        int[] Primero = new int[5];
+        int[] Segundo = new int[5];
+        int[] Resultado = new int[5];
 
-	}
+        // Llenar el primer arreglo
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Valor " + (i + 1) + " para el primer arreglo: ");
+            Primero[i] = Orlando.nextInt();
+        }
+
+        // Llenar el segundo arreglo (en orden inverso)
+        for (int i = 4; i >= 0; i--) {
+            System.out.print("Valor " + (i + 1) + " para el segundo arreglo (orden inverso): ");
+            Segundo[i] = Orlando.nextInt();
+        }
+
+        // Multiplicar los elementos correspondientes de ambos arreglos
+        for (int i = 0; i < 5; i++) {
+            Resultado[i] = Primero[i] * Segundo[i];
+        }
+
+        // Imprimir el resultado de la multiplicación
+        System.out.print("Resultado de la multiplicación de los arreglos: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(Resultado[i] + " ");
+        }
+    }
 }
+
