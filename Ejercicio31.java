ccb60c5f421c329ac42c64adf5e818dfeb6dc927
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-14 09:20:26 -0600

    Cafeteria del Tesji, sorteo de pelotas

--- Ejercicio31.java
+++ Ejercicio31.java
@@ -2,4 +2,6 @@
  * Fecha: 18-07-2023
  * Correo: urbanoorlando79@gmail.com
  
-  * Ejercicio: En la cafetería de Tesji hay una urna con pelotas de colores. verde, roja y amarilla. Cada una aplica un desucento diferente en el total de tu compra. La pelota verde ofrece un 10% de descuento, la amarilla un 5% y la roja un 15%. Con base a esos datos, imprime el monto final a pagar, ya con el descuento. *
+  * Ejercicio: En la cafetería de Tesji hay una urna con pelotas de colores. verde, roja y amarilla. Cada una aplica un desucento diferente en el total de tu compra. La pelota verde ofrece un 10% de descuento, la amarilla un 5% y la roja un 15%. Con base a esos datos, imprime el monto final a pagar, ya con el descuento. */
+
+import java.util.Scanner;
