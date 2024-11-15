f3c4a6bae24a495f07f3acbbebe6b7ccdb1b1198
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-15 10:34:53 -0600

    Uso de arreglos bidimensionales

--- Ejercicio32.java
+++ Ejercicio32.java
@@ -1,3 +1,5 @@
 /* Autor: Orlando Urbano Trejo (Lando)
  * Fecha: 18-07-2023
  * Correo: urbanoorlando79@gmail.com
+ 
+ * Ejercicio: Haciendo uso de arreglos bidimensionales, solicita la calificación de las siguientes materias:
